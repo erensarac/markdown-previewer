6aa636d3908c21022a0aae2b2e1c4bc613e15559
eren <esarac@outlook.com.tr>
2024-06-29 12:14:58 +0300

    fix: add base path to vite config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/markdown-previewer/",
   plugins: [vue(), vueJsx()],
   resolve: {
     alias: {
